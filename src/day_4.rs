0557ccf28edf891dd086cf03c2dfa57532ba6957
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-12-18 10:43:01 -0800

    day4

---
+++ src/day_4.rs
@@ -0,0 +1,58 @@
+use crate::common::get_input;
+
+fn is_sorted(array: &[i32]) -> bool {
+    if array.len() > 0 {
+        let mut prev = array[0];
+        for item in array {
+            if prev > *item {
+                return false;
+            }
+            prev = *item;
+        }
+    }
+    return true;
+}
+
+pub fn day_4() {
+    let input_string = get_input("/Users/harkunwar/advent_of_code/assets/input_4.txt");
+    let input_array = input_string.split('\n');
+    let mut complete_overlap_count = 0;
+    let mut overlap_count = 0;
+    for input in input_array {
+        let mut elf_works_raw = input.split(',');
+        let mut first_elf_raw = elf_works_raw.next().unwrap().split('-');
+        let mut second_elf_raw = elf_works_raw.next().unwrap().split('-');
+        let first_elf_work: [i32; 2] = [
+            first_elf_raw.next().unwrap().parse().unwrap(),
+            first_elf_raw.next().unwrap().parse().unwrap(),
+        ];
+        let second_elf_work: [i32; 2] = [
+            second_elf_raw.next().unwrap().parse().unwrap(),
+            second_elf_raw.next().unwrap().parse().unwrap(),
+        ];
+        if (first_elf_work[0] <= second_elf_work[0] && first_elf_work[1] >= second_elf_work[1])
+            || (first_elf_work[0] >= second_elf_work[0] && first_elf_work[1] <= second_elf_work[1])
+        {
+            complete_overlap_count += 1;
+            overlap_count += 1;
+            continue;
+        }
+        let overlap_order_1 = [
+            first_elf_work[0],
+            second_elf_work[0],
+            first_elf_work[1],
+            second_elf_work[1],
+        ];
+        let overlap_order_2 = [
+            second_elf_work[0],
+            first_elf_work[0],
+            second_elf_work[1],
+            first_elf_work[1],
+        ];
+        if is_sorted(&overlap_order_1) || is_sorted(&overlap_order_2) {
+            overlap_count += 1;
+        }
+    }
+    println!("Day 3, Part 1: {}", complete_overlap_count);
+    println!("Day 3, Part 2: {}", overlap_count);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod common;
 mod day_1;
-mod day_2;
-mod day_3;
 use day_1::day_1;
+mod day_2;
 use day_2::day_2;
+mod day_3;
 use day_3::day_3;
+mod day_4;
+use day_4::day_4;
 
 fn main() {
     day_1();
     day_2();
     day_3();
+    day_4();
 }
